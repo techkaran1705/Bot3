6df9a2bf4ae6182b7b82258652a60215935cc6c7
Velosh <daffetyxd@gmail.com>
2021-04-24 20:47:54 -0300

    Eval: Bring-up Eval command
    * Still in beta, imported from: https://github.com/HuTaoNetwork/HuTao/commit/a8a84445eca9233c68dcee09e5b3a228b1f71d70#diff-b94bcbd5676d5984086a683eb6347de9610a517e9a20070b298e30c0649ce0c9
    
    Signed-off-by: Velosh <daffetyxd@gmail.com>

---
+++ src/main/java/com/vegazsdev/bobobot/commands/owner/Eval.java
@@ -0,0 +1,67 @@
+package com.vegazsdev.bobobot.commands.owner;
+
+import com.vegazsdev.bobobot.TelegramBot;
+import com.vegazsdev.bobobot.core.Command;
+import com.vegazsdev.bobobot.db.PrefObj;
+import com.vegazsdev.bobobot.utils.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public class Eval extends Command {
+
+    private static final Logger logger = LoggerFactory.getLogger(Eval.class);
+
+    public Eval() {
+        super("eval", "Run java commands with one command");
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Override
+    public void botReply(Update update, TelegramBot bot, PrefObj prefs) {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        String[] msgComparableRaw = update.getMessage().getText().split(" ");
+
+        if (update.getMessage().getFrom().getId() == Float.parseFloat(Objects.requireNonNull(Config.getDefConfig("bot-master")))) {
+            try {
+                engine.eval("var imports = new JavaImporter(" +
+                        "java," +
+                        "java.io," +
+                        "java.lang," +
+                        "java.util," +
+                        "Packages.com.vegazsdev.bobobot.commands" +
+                        "Packages.org.telegram.telegrambots," +
+                        "Packages.org.telegram.telegrambots.meta.api.methods," +
+                        "Packages.org.telegram.telegrambots.meta.api.objects" +
+                        ");"
+                );
+
+                engine.put("args", msgComparableRaw);
+                engine.put("bot", bot);
+                engine.put("engine", engine);
+                engine.put("update", update);
+                engine.put("prefs", prefs);
+
+                String command = update.getMessage().getText().substring(msgComparableRaw[0].length());
+
+                Object out = engine.eval(
+                        "(function() {" +
+                                "with (imports) {\n" +
+                                command +
+                                "\n}" +
+                                "})();"
+                );
+
+                bot.sendMessage(out == null ? "Executed without error." : out.toString(), update);
+            } catch (Exception exception) {
+                bot.sendMessage("`" + exception.getMessage() + "`", update);
+                logger.error(exception.getMessage());
+            }
+        }
+    }
+}
