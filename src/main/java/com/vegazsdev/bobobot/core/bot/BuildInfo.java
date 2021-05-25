6242b066ce85012ea75869a3d8effbce75c31731
Velosh <daffetyxd@gmail.com>
2021-05-25 14:49:37 -0300

    Bot: Bump to v1.4.5

--- src/main/java/com/vegazsdev/bobobot/core/bot/BuildInfo.java
+++ src/main/java/com/vegazsdev/bobobot/core/bot/BuildInfo.java
@@ -15,7 +15,7 @@ public record BuildInfo(boolean doStable) {
     }
 
     private static class Variables {
-        public final String VERSION = "v1.4.1-";
+        public final String VERSION = "v1.4.5-";
         public final String STABLE = "STABLE";
         public final String STAGING = "BETA";
     }
