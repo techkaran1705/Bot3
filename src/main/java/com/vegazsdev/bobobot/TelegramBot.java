c60a850361217a73c2065de640c96035e7ca1c69
Velosh <daffetyxd@gmail.com>
2021-07-08 18:01:23 -0300

    TelegramBot: TelegramBots Library v5.3.0 Changes
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- src/main/java/com/vegazsdev/bobobot/TelegramBot.java
+++ src/main/java/com/vegazsdev/bobobot/TelegramBot.java
@@ -14,7 +14,7 @@ import org.telegram.telegrambots.meta.api.methods.groupadministration.LeaveChat;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.ChatMember;
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
